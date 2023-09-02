0f381fc98193745986d324831b7c4e731ded1626
oldTapokk <chelovek.228.tiktok@>
2023-09-02 17:45:17 +0300

    tyureb

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hgh')
+class Car:
+    color = "white"
+
+def __init__(self, model, color, year):
+    self.model = model
+    self.year = year
+    self.color = color
+def show_info(self):
+    print('model:', self.model)
+    print('year:', self.year)
+    print('color:', self.color)
+
+def set_color(self, newColor):
+    self.color = newColor
+
+
+myCar = Car('audi', '2021', 'red')
+myCar.color = 'red'
+myCar.show_info()
+
+myCar2 = Car('lamborgini', '1986', 'black')
+myCar2.color = 'black'
+myCar2.show_info()
